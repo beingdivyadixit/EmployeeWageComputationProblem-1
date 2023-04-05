e83257d00f4faecb7fe6dc3e4eb3fb2f154aaa10
Divya <beingdivyadixit@gmail.com>
2023-04-05 12:59:41 +0530

    UC-1

---
+++ EmployeeWageComputationProblem_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWageComputationProblem_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Employee Wage Computation Problem");
+        }
+    }
+}
