e0ae37143e382ddd246a11499d3a366f343a97d8
Divya <beingdivyadixit@gmail.com>
2023-04-05 14:57:39 +0530

    UC-2

--- EmployeeWageComputationProblem_1/EmployeeWadgeUC2.cs
+++ EmployeeWageComputationProblem_1/EmployeeWadgeUC2.cs
@@ -8,5 +8,21 @@ namespace EmployeeWageComputationProblem_1
 {
     internal class EmployeeWadgeUC2
     {
+        public static void EmployeeUC2(int empCheck)
+        {
+            int IS_FULL_TIME = 1, EMP_RATE_PER_HOUR = 20;
+            int empWadge = 0, empHr = 0;
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHr = 8;
+            }
+            else
+            {
+                empHr = 0;
+            }
+            empWadge = empHr * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wadge:" + empWadge);
+        }
     }
 }
