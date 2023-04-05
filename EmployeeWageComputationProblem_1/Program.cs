19c762fe2e510de55cbda2efb9a30b3872173757
Divya <beingdivyadixit@gmail.com>
2023-04-05 15:10:10 +0530

    UC-3

--- EmployeeWageComputationProblem_1/Program.cs
+++ EmployeeWageComputationProblem_1/Program.cs
@@ -6,10 +6,9 @@ namespace EmployeeWageComputationProblem_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Enter 0 for welcome program \n Enter 1 for UC1 \n Enter 2 for UC2");
+            Console.WriteLine(" Enter 0 for welcome program \n Enter 1 for UC1 \n Enter 2 for UC2 \n Enter 3 for UC3");
             int option = Convert.ToInt32(Console.ReadLine());
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
             switch (option)
             {
 
@@ -17,10 +16,16 @@ namespace EmployeeWageComputationProblem_1
                     Console.WriteLine("Welcome to Employee Wage Computation Program");
                     break;
                 case 1:
-                     EmployeeWadgeUC1.EmployeeUC1(empCheck);
+                    int empCheck1 = random.Next(0, 2);
+                    EmployeeWadgeUC1.EmployeeUC1(empCheck1);
                     break;
                 case 2:
-                    EmployeeWadgeUC2.EmployeeUC2(empCheck);
+                    int empCheck2 = random.Next(0, 2);
+                    EmployeeWadgeUC2.EmployeeUC2(empCheck2);
+                    break;
+                case 3:
+                    int empCheck3 = random.Next(0, 3);
+                    EmployeeWadgeUC3.EmployeeUC3(empCheck3);
                     break;
                 default:
                     Console.WriteLine("Please Input Correct option");
